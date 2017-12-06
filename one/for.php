6eab250eac72926d4ce4c883945319bcbf9f72e0
hantao <newbieto@163.com>
2017-12-06 16:46:45 +0800

    for foreach while

--- one/for.php
+++ one/for.php
@@ -13,9 +13,17 @@
 		echo $i;
 		echo "</br>";
 	}
-
-
-
+	// foreach
+	$arr = array(
+		0 => "苹果",
+		1 => "香蕉",
+		2 => "鸭梨"
+	);
+	foreach($arr as $key => $value){
+		echo $key ."=>". $value;
+		echo "</br>";
+	}
+	// 
 
 
 
