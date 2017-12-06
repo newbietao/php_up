3115032a11114ca83ec15c2ee5dca341f863fa36
hantao <newbieto@163.com>
2017-12-06 18:05:39 +0800

    class

---
+++ two/class.php
@@ -0,0 +1,27 @@
+<?php
+	// 声明一个类
+	class Car {
+		public $name = "baoma";
+		public function getName() {
+			return $this->name;
+		}
+	
+	}
+	$car = new Car();
+	echo $car->getName();
+	// 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
