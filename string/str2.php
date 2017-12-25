f996b9f510f482c994267a1e6546e3f766ae2d98
hantao <newbieto@163.com>
2017-12-25 11:09:28 +0800

    trim

--- string/str2.php
+++ string/str2.php
@@ -6,6 +6,11 @@
 	for($i=0;$i<$len;$i++){
 		echo "{$i}th is {$str[$i]}\n";
 	}
-
+	// 字符串函数
+	// trim() 删除字符串首尾空格，ltrim删除左边空格，rtrim删除右边空格
+	$str = "    hello   ";
+	echo trim($str)."\n";
+	echo ltrim($str)."\n";
+	echo rtrim($str)."\n";
 
 ?>
