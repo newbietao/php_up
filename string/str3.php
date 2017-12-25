53a9dd4d0f43e9927deb972c820856e8463b77aa
hantao <newbieto@163.com>
2017-12-25 14:14:20 +0800

    用substr_replace实现替换、插入、删除

--- string/str3.php
+++ string/str3.php
@@ -4,7 +4,18 @@
 	echo substr($str,6,5)."\n";
 	// 计算子字符串在父字符串中出现的次数
 	echo substr_count($str,"l")."\n";
-	
+	// 字符串长度
+	echo strlen($str)."\n";// 返回字符串长度
+	// 子字符串替换,不改变原字符串
+	// 替换
+	echo substr_replace($str,"hantao",6,5)."\n";
+	// 删除
+	echo substr_replace($str,"",6,5)."\n";
+	// 插入
+	echo substr_replace($str,"hantao",6,0)."\n";
+
+
+
 
 
 
