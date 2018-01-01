4443c28657618e6f5be89006f263b5338e480d37
hantao <newbieto@163.com>
2018-01-01 16:09:18 +0800

    construct curl_close reset

--- cUrl/curl_class.php
+++ cUrl/curl_class.php
@@ -8,29 +8,41 @@
 class Curl {
 	private $url = "";
 	private $data = "";
-	
+	private $curl = "";
 	public function __construct($url, $data = array()) {
-		
+		$this->url = $url;
+		$this->data = $data;
+		$this->curl = curl_init();//初始化curl
+		curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, true);
 	}
-	public function curl_reset() {
-	
+	public function curl_reset($key,$value) {
+		
+		curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, true);
 	}
 	public function get() {
 	
+		return $this->close_curl();
 	}
 	public function post() {
 	
+		return $this->close_curl();
 	}
 	public function ftp_download() {
 	
+		return $this->close_curl();
 	}
 	public function ftp_upload() {
 	
+		return $this->close_curl();
 	}
 	private function doRequest() {
 	
 	}
-	
+	private function close_curl() {
+		$output=curl_exec($this->curl);	// 执行
+		curl_close($this->curl);			// 关闭cURL
+		return $output;
+	}
 }
 
 
