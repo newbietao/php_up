1b0ff2148b67c08e0d28478d2a7044d0f927c4a1
hantao <newbieto@163.com>
2018-01-01 15:56:27 +0800

    curl类的基本架构

---
+++ cUrl/curl_class.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @author hantao
+ * @date 2018-01-01
+ * @为了纪念我苦逼、无聊、单身的2018年元旦
+ */
+
+class Curl {
+	private $url = "";
+	private $data = "";
+	
+	public function __construct($url, $data = array()) {
+		
+	}
+	public function curl_reset() {
+	
+	}
+	public function get() {
+	
+	}
+	public function post() {
+	
+	}
+	public function ftp_download() {
+	
+	}
+	public function ftp_upload() {
+	
+	}
+	private function doRequest() {
+	
+	}
+	
+}
+
+
+
+
+
+?>
