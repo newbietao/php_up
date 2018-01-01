0763e6eaaf69f7ec36aaf55dc838bc71f9625b14
hantao <newbieto@163.com>
2018-01-01 18:43:57 +0800

    post opencookie ftp_upload

--- cUrl/curl_class.php
+++ cUrl/curl_class.php
@@ -19,43 +19,70 @@ class Curl {
 	public function curl_set($option) {
 		if(is_array($option)){
 			foreach($option as $key => $value){
-				curl_setopt($this->curl, constant($key), $value);
+				curl_setopt($this->curl, constant(strtoupper($key)), $value);
 			}
 		}else{
 			exit('option master be a array');
 		}
 	}
+
 	public function get() {
-	
-		return $this->close_curl();
+		return curl_exec($this->curl);
 	}
-	public function post() {
-	
-		return $this->close_curl();
+
+	public function post($data) {
+		$this->data = isset($data) ? $data : $this->data;
+		curl_setopt($this->curl, CURLOPT_POST, TRUE);  
+		curl_setopt($this->curl, CURLOPT_POSTFIELDS, $this->data);  
+		curl_setopt($this->curl, CURLOPT_HTTPHEADER, array("application/x-www-form-urlencoded; charset=utf-8", 
+			"Content-length: ".strlen($this->data)
+				)); 
+		return curl_exec($this->curl);
 	}
+
+	public function open_cookie() {
+		date_default_timezone_set('PRC'); // 使用Cookie时，必须先设置时区
+		curl_setopt($curlobj, CURLOPT_COOKIESESSION, TRUE); 
+		curl_setopt($curlobj, CURLOPT_HEADER, FALSE);
+	}
+
 	public function ftp_download() {
 	
-		return $this->close_curl();
+		
 	}
-	public function ftp_upload() {
-	
-		return $this->close_curl();
+
+	public function ftp_upload($url, $localfile, $user, $passwd, $timeout=300) {
+		$fp = fopen($localfile, 'r');
+
+		curl_setopt($this->curl, CURLOPT_URL, $url);  
+		curl_setopt($this->curl, CURLOPT_HEADER, FALSE); 
+		curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, TRUE);  
+		curl_setopt($this->curl, CURLOPT_TIMEOUT, $timeout); // times out after 300s
+		curl_setopt($this->curl, CURLOPT_USERPWD, $user.":".$passwd);//FTP用户名：密码
+
+		curl_setopt($this->curl, CURLOPT_UPLOAD, TRUE);
+		curl_setopt($this->curl, CURLOPT_INFILE, $fp);
+		curl_setopt($this->curl, CURLOPT_INFILESIZE, filesize($localfile));
+		$rtn = curl_exec($this->curl);  
+		fclose($fp); 
+		
+		if(!curl_errno($this->curl)){
+			echo "Uploaded successfully.";  
+		} else {
+			echo 'Curl error: ' . curl_error($curlobj);
+		}
 	}
-	private function doRequest() {
-	
-	}
-	private function close_curl() {
-		$output=curl_exec($this->curl);	// 执行
+	public function close_curl() {	
 		curl_close($this->curl);			// 关闭cURL
-		return $output;
+	}
+	public function request() {
+	
+		
 	}
 }
 
-$t = new Curl("http://www.baidu.com");
-$t->curl_set(array(
-	"CURLOPT_URL" => "http://www.cnblogs.com/freephp/p/5283239.html"
-));
-echo $t -> get();
+
+
 
 
 
