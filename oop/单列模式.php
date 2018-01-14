e4cc2c13c04b443dcdc0baa3c6355d10794f0c03
hantao <newbietao@163.com>
2018-01-14 12:57:19 +0800

    单列模式

--- oop/单列模式.php
+++ oop/单列模式.php
@@ -5,20 +5,24 @@
 	class Single {
 		private static $_instance = null;
 		private function __construct() {
-			echo "单例模式的构造函数",'\n';
+			echo "单例模式的构造函数","</br>";
 		}
 		public function __destruct() {
-			echo "单例模式的析构函数",'\n';
+			echo "单例模式的析构函数","</br>";
 		}
 		public static function getInstance() {
 			if(is_null(self::$_instance)){
 				$class = __CLASS__;
 				self::$_instance = new $class();
-			}
+		}
 			return self::$_instance;
+		}
+		public function test() {
+			echo "test","</br>";
 		}
 
 	}
 	$test = Single::getInstance();
+	$test->test();
 
 ?>
