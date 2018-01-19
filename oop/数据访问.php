ac080763914067ca9db9144f1e2a114e12d4f7b1
hantao <newbietao@163.com>
2018-01-19 20:07:27 +0800

    parent 关键字

---
+++ oop/数据访问.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hantao
+ * Date: 18-1-18
+ * Time: 下午9:06
+ */
+class Parents {
+    public function __construct()
+    {
+        echo "父类构造函数\n";
+    }
+    public function test() {
+        echo "test \n";
+    }
+    public $name = "hantao";
+    public static $age = 23;
+    public static function test1() {
+        echo "test1 \n";
+    }
+}
+
+class Child extends Parents {
+    public function __construct()
+    {
+        /**
+         * parent 关键字能访问的数据
+         * @1 父类的静态属性
+         * @2 父类的静态方法
+         * @3 父类的公有方法
+         * @4 父类的受保护方法
+         */
+        // parent 关键字访问父类的构造函数
+        parent::__construct();
+        // parent 关键字访问父类的方法
+        parent::test();
+        // parent 关键字访问父类的静态方法
+        parent::test1();
+        // parent 关键字访问父类的静态属性
+        echo parent::$age,"\n";
+        // parent 关键字访问父类的属性
+        // echo parent::$name,"\n"; 报错
+
+    }
+
+
+
+}
+$test = new Child();
