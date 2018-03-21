76f288cc890979635ddc3125bf1cd6899eeea152
hantao <newbietao@163.com>
2018-03-22 07:49:02 +0800

    substr的使用

---
+++ string/substr.php
@@ -0,0 +1,20 @@
+<?php
+$str = "hello world";
+echo substr($str,1);
+echo "\n";
+echo substr($str,2);
+echo "\n";
+echo substr($str,3);
+echo "\n";
+echo substr($str,-3);
+echo "\n";
+
+// length 的使用
+echo substr($str,1,3);
+echo "\n";
+echo substr($str,1,-3);
+echo "\n";
+
+
+
+?>
