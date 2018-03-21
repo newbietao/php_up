5c92a299573ca7614c3e20da459125cf4838757d
hantao <newbietao@163.com>
2018-03-22 07:57:57 +0800

    substr_count

---
+++ string/substr_count.php
@@ -0,0 +1,12 @@
+<?php
+$str = "this is a dog";
+echo strlen($str);
+echo "\n";
+echo substr_count($str, "is");
+echo "\n";
+echo substr_count($str, "is", 3);
+echo "\n";
+echo substr_count($str, "is", 3, 3);
+echo "\n";
+
+?>
