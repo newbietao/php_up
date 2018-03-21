1218823eea1dd1c25eb7c1ac93c3b5691814d970
hantao <newbietao@163.com>
2018-03-21 18:57:53 +0800

    string to something

---
+++ string/str_to_x.php
@@ -0,0 +1,17 @@
+<?php
+// string to lower
+$str = "HELLO WORLD";
+var_dump($str);
+var_dump(strtolower($str));
+// string to upper
+$str = "hello world";
+var_dump($str);
+var_dump(strtoupper($str));
+// string to ucfirst
+$str = "hello world";
+var_dump($str);
+var_dump(ucfirst($str));
+// string to ucwords
+var_dump(ucwords($str))
+
+?>
